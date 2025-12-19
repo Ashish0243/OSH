08c47d70d78de40112454207756461709c38a1bd
ASHISH RAJPAL <ashishrajpal73@gmail.com>
2025-12-19 23:39:54 +0530

    github_logic

--- techstack/fetch_project_data.py
+++ techstack/fetch_project_data.py
@@ -29,7 +29,7 @@ def fetch_project_tech_topics():
         }
         
         project_tech_topic_data.append(
-            Document( page_content=json.dumps(project_data), 
+            Document( page_content=json.dumpse(project_data), 
             metadata={"project_id": str(project.id)}
             ))
     
